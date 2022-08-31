e57add1a044d165c465cf111dfb3d0205d288f70
Mustafa Binalhag <mbin828@gmail.com>
2022-08-31 17:28:05 -0400

    crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,54 @@ class ApplicationController < Sinatra::Base
   get '/' do
     { message: "hello #{Faker::Name.first_name } this is the root route"}.to_json
   end
-  get '/users' do
-    User.all.to_json
-  end
-  
   get '/fruits' do
     Fruit.all.to_json
   end
   
+  get '/users' do
+    User.all.to_json
+  end
   
+  get '/users/:id' do
+    user_data = User.find(params[:id])
+    # i had to parse it into json twice because the out putting string in 
+    # {
+    #   user: user_data.to_json 
+    # }
+    # looked like 
+    # \"id\":26,\"name\":\"Donquixote Doflamingo\",\"email\":\"DonquixoteDoflamingo@bounty.com\",
+
+    # there is probably a better way to do this
+    {
+      user:JSON.parse( user_data.to_json),
+      fruits:JSON.parse( user_data.fruits.to_json)
+    }.to_json
+  end
+
+  post "/eat-devil-fruit" do
+    data = JSON.parse request.body.read
+    Fruit.create(name: data['devil_fruit'], user_id:data['user_id'] )
+    user_data = User.find(data['user_id'])
+    {
+      user:JSON.parse( user_data.to_json),
+      fruits:JSON.parse( user_data.fruits.to_json)
+    }.to_json
+  end
+  
+  delete "/destory-user/:id" do
+    user_data = User.find(params[:id])
+    user_data.destory
+    user_data.to_json
+  end
+
+  put "/update_user" do
+    data = JSON.parse request.body.read
+    user_data = User.find(data['user_id'])
+    # if data["key"] is empty default to old values
+    user_data.name = data['name'] || user_data.name
+    user_data.email = data['email'] || user_data.email
+    user_data.password = data['password'] || user_data.password
+    user_data.save
+    user_data.to_json
+  end
 end
